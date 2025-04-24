9d90debca74eee6524595532cad5d5c73074690c
Wenyu Zhao <wenyuzhaox@gmail.com>
2025-04-24 22:48:26 +1000

    Update exports

--- agentia/__init__.py
+++ agentia/__init__.py
@@ -12,7 +12,47 @@ def init_logging(level: logging._Level = logging.INFO):
     logging.getLogger("httpx").setLevel(logging.WARNING)
 
 
-from .agent import Agent
 from . import message
 from . import plugins
 from . import utils
+
+from .agent import Agent
+from .message import (
+    Message,
+    Event,
+    UserMessage,
+    SystemMessage,
+    AssistantMessage,
+    UserConsentEvent,
+    ToolCallEvent,
+    CommunicationEvent,
+)
+from .chat_completion import ChatCompletion, MessageStream, ReasoningMessageStream
+from .plugins import Plugin, tool
+
+__all__ = [
+    # submodules
+    "message",
+    "plugins",
+    "utils",
+    # agent
+    "Agent",
+    # message
+    "Message",
+    "Event",
+    "UserMessage",
+    "SystemMessage",
+    "AssistantMessage",
+    "UserConsentEvent",
+    "ToolCallEvent",
+    "CommunicationEvent",
+    # chat_completion
+    "ChatCompletion",
+    "MessageStream",
+    "ReasoningMessageStream",
+    # plugins
+    "Plugin",
+    "tool",
+    # utils
+    "init_logging",
+]
