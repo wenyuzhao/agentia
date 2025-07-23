b678ccb9baa81bcee9f1d21acf946ea009090b30
Wenyu Zhao <wenyuzhaox@gmail.com>
2025-07-23 21:41:53 +1000

    Use pydantic for history object

--- agentia/history.py
+++ agentia/history.py
@@ -1,51 +1,48 @@
-from pathlib import Path
-from typing import TYPE_CHECKING, Union
-import shelve
+from typing import Annotated, Union
+from pydantic import BaseModel, Field
 
-from .message import Message, SystemMessage, is_message
-
-if TYPE_CHECKING:
-    from .agent import Event
+from .message import Message, SystemMessage, is_message, Event
 
 
-class History:
+class History(BaseModel):
+    instructions: str | None = None
+    messages: list[
+        Annotated[Union[Message, "Event"], Field(union_mode="left_to_right")]
+    ] = Field(default_factory=list)
+    summary: str | None = None
+
     def __init__(self, instructions: str | None) -> None:
-        self._instructions = instructions
-        self.__messages: list[Union[Message, "Event"]] = []
-        self.summary = None
-        self.__first_conversation_finished = False
+        super().__init__(
+            instructions=instructions,
+            messages=[],
+            summary=None,
+        )
         self.reset()
 
     def update_summary(self, summary: str) -> None:
         self.summary = summary
 
     def add_instructions(self, instructions: str) -> None:
-        if self._instructions is None:
-            self._instructions = instructions
+        if self.instructions is None:
+            self.instructions = instructions
         else:
-            self._instructions += "\n\n" + instructions.strip()
+            self.instructions += "\n\n" + instructions.strip()
 
     def get_for_inference(self) -> list[Message]:
         """
         Get the recent messages for inference
         """
-        messages = History.__filter(self.__messages)
+        messages = History.__filter(self.messages)
         return messages
 
     def reset(self):
-        self.__messages = []
-        if self._instructions is not None:
-            self.add(SystemMessage(content=self._instructions))
+        self.messages = []
+        if self.instructions is not None:
+            self.add(SystemMessage(content=self.instructions))
 
     def add(self, message: Union[Message, "Event"]):
         # TODO: auto trim history
-        self.__messages.append(message)
-
-    def set(self, messages: list[Union[Message, "Event"]]):
-        self.__messages = messages
-
-    def get(self) -> list[Union[Message, "Event"]]:
-        return self.__messages
+        self.messages.append(message)
 
     @staticmethod
     def __filter(
@@ -55,43 +52,6 @@ class History:
         Filter out non-message events
         """
         return [m for m in msgs if is_message(m)]
-
-    def _first_conversation_just_finished(self) -> bool:
-        from .message import AssistantMessage, UserMessage
-
-        if self.__first_conversation_finished:
-            return False
-        has_assistant = (
-            len([m for m in self.__messages if isinstance(m, AssistantMessage)]) > 0
-        )
-        has_user = len([m for m in self.__messages if isinstance(m, UserMessage)]) > 0
-        finished = has_assistant and has_user
-        self.__first_conversation_finished = finished
-        return finished
-
-    def _save(self, path: Path) -> None:
-        """
-        Save the history to a file
-        """
-        path.parent.mkdir(parents=True, exist_ok=True)
-        from .message import AssistantMessage
-
-        with shelve.open(path) as db:
-            db["messages"] = self.__messages
-            if self.summary:
-                db["summary"] = self.summary
-
-    def _load(self, path: Path) -> None:
-        """
-        Load the history from a file
-        """
-        if not path.exists():
-            return
-        with shelve.open(path) as db:
-            if "messages" in db:
-                self.__messages = db["messages"]
-            if "messages" in db:
-                self.summary = db.get("summary", None)
 
     def get_formatted_history(self) -> str:
         """
@@ -105,7 +65,7 @@ class History:
         )
 
         S = ""
-        for m in self.__messages:
+        for m in self.messages:
             match m:
                 case UserMessage(content=content):
                     if isinstance(content, str):
