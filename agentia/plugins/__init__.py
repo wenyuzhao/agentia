a1b3d591e54b1abb93adb6b951f5f248a9f7c867
Wenyu Zhao <wenyuzhaox@gmail.com>
2026-04-02 16:46:41 +1100

    Remove skill_learner.py

--- agentia/plugins/__init__.py
+++ agentia/plugins/__init__.py
@@ -13,7 +13,6 @@ from .calc import Calculator
 from .clock import Clock
 from .code_runner import CodeRunner
 from .skills import Skills
-from .skill_learner import SkillLearner
 from .memory import Memory
 from .search import Search
 from .web import Web
